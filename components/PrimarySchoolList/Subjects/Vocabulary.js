a436a437d8fc120e720f9ed17f0708760c52ca1a
kingawr123 <kingawr123@gmail.comm>
2019-06-21 11:38:33 +0200

    vocabulary list

--- components/PrimarySchoolList/Subjects/Vocabulary.js
+++ components/PrimarySchoolList/Subjects/Vocabulary.js
@@ -2,24 +2,50 @@ import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { fonts, baseColors } from '../../../styles/base';
 import { SubHeader } from '../../SubHeader';
-import { listOfSubsInPrimarySchool } from '../PrimarySchoolList';
-
+import { ScrollView, FlatList } from 'react-native-gesture-handler';
+import { padd } from '../../../styles/base'
 
 export class Vocabulary extends React.Component {
+    
+    listOfVocabulary = [
+        {
+            value: 'eat',
+            key: 'jeść'
+        },
+        {
+            value: 'sleep',
+            key: 'spać'
+        }
+    ]
+
+    renderItem({ item }){
+        return <View style={styles.textInListContainer}><Text style={styles.text}>{item.key} - {item.value}</Text></View>
+    }
+
     render(){
         return(
             <View>
-            <SubHeader title="Podstawowe słownictwo"/>
+                <SubHeader title="Podstawowe słownictwo"/>
             <View>
-                <Text>Ogólnie znana teza głosi, iż użytkownika może rozpraszać zrozumiała zawartość strony, kiedy ten chce zobaczyć sam jej wygląd. Jedną z mocnych stron używania Lorem Ipsum jest to, że ma wiele różnych „kombinacji” zdań, słów i akapitów, w przeciwieństwie do zwykłego: „tekst, tekst, tekst”, sprawiającego, że wygląda to „zbyt czytelnie” po polsku.</Text>
+            <FlatList
+                data={this.listOfVocabulary}
+                renderItem={this.renderItem.bind(this)}>
+            </FlatList> 
             </View>
             </View>
         )
     }
 }
 
-// const styles = StyleSheet.create({
-//     textCon: {
-//         backgroundColor: 'red',
-//     }
-// })
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 30,
+        color: baseColors.mainHeader
+    },
+
+    textInListContainer:{
+        borderBottomWidth: .5,
+        borderBottomColor: 'black',
+        padding: padd.listItem,
+    }
+})
