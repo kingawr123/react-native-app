16cdc3f1952ed0f3f34f12620327654db59dce59
kingawr123 <kingawr123@gmail.comm>
2019-07-03 16:13:55 +0200

    list in JSON

--- components/PrimarySchoolList/Subjects/Vocabulary.js
+++ components/PrimarySchoolList/Subjects/Vocabulary.js
@@ -3,35 +3,18 @@ import { Text, View, StyleSheet } from 'react-native';
 import { fonts, baseColors } from '../../../styles/base';
 import { SubHeader } from '../../SubHeader';
 import { ScrollView, FlatList } from 'react-native-gesture-handler';
-import { padd } from '../../../styles/base'
+import { padd } from '../../../styles/base';
+import  baseVocabulary from '../../../data/baseVocabulary.json'
 
 export class Vocabulary extends React.Component {
-
-      listOfVocabulary = [
-        {
-            value: 'eat',
-            key: 'jeść'
-        },
-        {
-            value: 'sleep',
-            key: 'spać'
-        }
-    ]
-
-    renderItem({ item }){
-        return <View style={styles.textInListContainer}><Text style={styles.text}>{item.key} - {item.value}</Text></View>
-    }
 
     render(){
         return(
             <View>
-                <SubHeader title="Podstawowe słownictwo"/>
-            <View>
-            <FlatList
-                data={this.listOfVocabulary}
-                renderItem={this.renderItem.bind(this)}>
-            </FlatList> 
-            </View>
+            <SubHeader title="Podstawowe słownictwo"/>
+            {baseVocabulary.map((vocabulary, index) => {
+                return <View style={styles.textInListContainer} key={index}><Text style={styles.text}>{vocabulary.key} - {vocabulary.value}</Text></View>
+            })}
             </View>
         )
     }
@@ -39,7 +22,7 @@ export class Vocabulary extends React.Component {
 
 const styles = StyleSheet.create({
     text: {
-        fontSize: 30,
+        fontSize: 20,
         color: baseColors.mainHeader
     },
 
