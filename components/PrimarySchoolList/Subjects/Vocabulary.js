7955a3c2b9497afcddfcabcee7cb4846510863e5
Kinga <kingawr123@gmail.com>
2019-07-18 16:11:41 +0200

    list

--- components/PrimarySchoolList/Subjects/Vocabulary.js
+++ components/PrimarySchoolList/Subjects/Vocabulary.js
@@ -1,19 +1,47 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import { fonts, baseColors } from '../../../styles/base';
+import { fonts, baseColors, padd, dimensions } from '../../../styles/base';
 import { SubHeader } from '../../SubHeader';
 import { ScrollView, FlatList } from 'react-native-gesture-handler';
-import { padd } from '../../../styles/base';
-import  baseVocabulary from '../../../data/baseVocabulary.json'
+import  baseVocabulary from '../../../data/baseVocabulary.json';
+import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export class Vocabulary extends React.Component {
+     ShowHideCom(item) {
+        if(item.show == false){
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
 
     render(){
         return(
             <View>
             <SubHeader title="Podstawowe słownictwo"/>
-            {baseVocabulary.map((vocabulary, index) => {
-                return <View style={styles.textInListContainer} key={index}><Text style={styles.text}>{vocabulary.key} - {vocabulary.value}</Text></View>
+            {baseVocabulary.map((item, index) => {
+                return <View style={styles.textInListContainer} key={index}>
+                <Text 
+                style={styles.text2}>
+                {item.fL}
+                </Text>
+                <Button 
+                type='clear'
+                icon={
+                    <Icon
+                    name="angle-down"
+                    color="grey"
+                    size={50}/>
+                }
+                onPress={() => item.show = !item.show}
+                containerStyle={styles.btn}/>
+                {
+                    item.show ? (<Text style={styles.text}>{item.vocabulary.key} - {item.vocabulary.value}</Text>) : null
+                }
+             
+                    </View>
             })}
             </View>
         )
@@ -23,12 +51,26 @@ export class Vocabulary extends React.Component {
 const styles = StyleSheet.create({
     text: {
         fontSize: 20,
-        color: baseColors.mainHeader
+        color: baseColors.mainHeader,
+        paddingLeft: 20
+    },
+
+    text2: {
+        color: 'black',
+        padding: 0,
+        margin: 0,
+        fontSize: 25,
     },
 
     textInListContainer:{
         borderBottomWidth: .5,
         borderBottomColor: 'black',
         padding: padd.listItem,
+    },
+    
+    btn: {
+        width: 80,
+        position: 'absolute',
+        left: dimensions.fullWidth - 80
     }
 })
